898fc6691b3d354ebb261112656909168e6da502
Robert Jonczy <robert.jonczy@gmail.com>
2016-02-04 23:20:58 +0100

    added some additional comments

--- anomaly-detection.R
+++ anomaly-detection.R
@@ -1,15 +1,18 @@
 # anomallly detection based on twitters package AnomalyDetection
+# https://github.com/twitter/AnomalyDetection
 
 #install.packages("devtools")
 #devtools::install_github("twitter/AnomalyDetection")
 library(AnomalyDetection)
 
+# get help
 help(package = 'AnomalyDetection')
 help(AnomalyDetectionTs)
 help(AnomalyDetectionVec)
 
-
+# load example data
 data(raw_data)
+
 res <- AnomalyDetectionTs(raw_data, max_anoms=0.02, direction='both', plot=TRUE)
 res$plot
 
@@ -17,3 +20,4 @@ AnomalyDetectionVec(raw_data[,2], max_anoms=0.02, period=1440, direction='both',
 
 res <- AnomalyDetectionTs(raw_data, max_anoms=0.02, direction='both', only_last='day', plot=TRUE)
 res$plot
+
