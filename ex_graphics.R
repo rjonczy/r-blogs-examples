fd2cec57d2f9c7acffe5ebce8329782bf69f0ef1
Robert Jonczy <robert.jonczy@gmail.com>
2016-02-24 23:40:42 +0100

    Revert "added example of graphics"
    This reverts commit 38c0cdff70f41f401d0690b020c281478b2c9647.

--- ex_graphics.R
+++
@@ -1,2 +0,0 @@
-# graphics examples
-# http://motioninsocial.com/tufte/
--- xgboost-example.R
+++
@@ -1,2 +0,0 @@
-# Uderstanding XGBoost on Otto model
-#https://www.kaggle.com/tqchen/otto-group-product-classification-challenge/understanding-xgboost-model-on-otto-data
